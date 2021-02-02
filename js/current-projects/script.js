bffa7ab715ad2eb54dc0edc004f03d26e78d0393
Jose Fon <fonjosev@gmail.com>
2021-02-01 17:33:57 -0800

    functions 2

--- js/current-projects/script.js
+++ js/current-projects/script.js
@@ -35,3 +35,44 @@
 // let r = sum(a + 1);
 // console.log(r);
 
+function sum2(a, b) {
+    return a + b;
+}
+
+let suma = sum2(1, 2);
+console.log(suma);
+
+function multiNum(a, b) {
+    return a * b;
+}
+
+let multiResult = multiNum(2, 3);
+console.log(multiResult);
+
+function divNum(a, b) {
+    return a / b;
+}
+
+let division = divNum(10, 2);
+console.log(division);
+
+function addWords (a, b) {
+    return a + b;
+}
+
+let wordsAdded = addWords("hola ", "chau");
+console.log(wordsAdded);
+
+function watchBrand(a, b) {
+    return "Best watches: " + a + " and " + b;
+}
+
+let twoBrands = watchBrand("Seiko", "Orient");
+console.log(twoBrands);
+
+function groceryStores(a, b) {
+    return a + " and " + b;
+}
+
+let twoStores = groceryStores("Safeway", "Albertsons");
+console.log(twoStores);
