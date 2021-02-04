85f373fc9fc748a5b79f730228c8f02bc09e6f82
Jose Fon <fonjosev@gmail.com>
2021-02-04 05:40:07 -0800

    a

--- js/current-projects/script.js
+++ js/current-projects/script.js
@@ -1,130 +1,13 @@
-// // function n(say) {
-// //     return say;
-// // }
-
-// // let s = n("Hello");
-// // let s2 = (s + " Jose");
-// // console.log(s2);
-
-// // function sayHi(name) {
-// //     return "Hello " + name;
-// // }
-
-// // let result = sayHi("Jose");
-// // console.log(result);
-
-// // function dilo(nombre) {
-// //     return "Hola " + nombre;
-// // }
-
-// // let h = dilo("Jose");
-// // console.log(h);
-
-// // function addName(name) {
-// //     return "Hello " + name;
-// // }
-
-// // let n = addName("Jose");
-// // console.log(n);
-
-// // function sum(a) {
-// //     return a;
-// // }
-
-// // let a = 1;
-// // let r = sum(a + 1);
-// // console.log(r);
-
-// function sum2(a, b) {
-//     return a + b;
-// }
-
-// let suma = sum2(1, 2);
-// console.log(suma);
-
-// function multiNum(a, b) {
-//     return a * b;
-// }
-
-// let multiResult = multiNum(2, 3);
-// console.log(multiResult);
-
-// function divNum(a, b) {
-//     return a / b;
-// }
-
-// let division = divNum(10, 2);
-// console.log(division);
-
-// function addWords (a, b) {
-//     return a + b;
-// }
-
-// let wordsAdded = addWords("hola ", "chau");
-// console.log(wordsAdded);
-
-// function watchBrand(a, b) {
-//     return "Best watches: " + a + " and " + b;
-// }
-
-// let twoBrands = watchBrand("Seiko", "Orient");
-// console.log(twoBrands);
-
-// function groceryStores(a, b) {
-//     return a + " and " + b;
-// }
-
-// let twoStores = groceryStores("Safeway", "Albertsons");
-// console.log(twoStores);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function printVariable(variable) {
     console.log(variable);
 }
 
 function func(x) {
-    x("Hello World");
+    return x;
 }
 
-func(printVariable);
+let a = func(2);
+console.log(a);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//-----------------
 
