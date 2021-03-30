036f96c74f7e512f749dda23c54f7d110b2ff1e1
Jose Fon <fonjosev@gmail.com>
2021-03-29 21:38:46 -0700

    more practice

--- js/current-projects/script.js
+++ js/current-projects/script.js
@@ -7,3 +7,51 @@ function handleSum(sum) {
 }
 
 sumCallback(1, 2, handleSum);
+
+/////////////////////////////////////////////
+
+function sayHello(yourName, callback) {
+    callback("Hello " + yourName);
+}
+
+function sayHola(texto) {
+    console.log(texto);
+}
+
+sayHello("Jose", sayHola);
+
+//////////////////////////////////////////////
+
+function house(xcolor, callback) {
+    callback(xcolor + " house");
+}
+
+function thisColor(house) {
+    console.log(house);
+}
+
+house("red", thisColor);
+
+///////////////////////////////////////////
+
+function addLetters(x, y, callback) {
+    callback(x + y);
+}
+
+function addThese(sum) {
+    console.log(sum);
+}
+
+addLetters("a" + " b", addThese); 
+
+////////////////////////////////////////
+
+function addWords(uno, dos, callback){
+    callback(uno + dos);
+}
+
+function twoWords(sum){
+    console.log(sum);
+}
+
+addWords("La " + "guitarra", twoWords);
