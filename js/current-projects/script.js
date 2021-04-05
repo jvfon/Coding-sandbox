79f2711e72be34b3ef145a7dbfa88584f54f2335
Jose Fon <fonjosev@gmail.com>
2021-04-04 18:55:13 -0700

    days

--- js/current-projects/script.js
+++ js/current-projects/script.js
@@ -6,10 +6,3 @@ watchFace("white", function(var1){
     console.log(var1);
 })
 ///////////////////////////////////////////
-function choco(late, callback){
-    callback("Hersheys? " + late);
-}
-
-choco("yes", function(var2){
-    console.log(var2);
-})
